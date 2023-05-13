f1f7d364d1a51ec40355b351fc17629a6693d1f7
armanpinachyan <pinachyan99@mail.ru>
2023-05-13 16:07:31 +0400

    ubdate

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import "./Login.css";
+import "./login.css";
 import { Button } from "@mui/material";
 import kompuslogo from "../../assets/images/kompuslogo.png";
+import { Link } from "react-router-dom";
+import { SITE_URL } from "../../utils/const";
 
 function Login() {
   return (
@@ -17,13 +19,15 @@ function Login() {
             email@example.com
           </a>
         </p>
-        <div className="Logininput">
+        <div className="LoginInput">
           <input type="text" placeholder="Кодовое слово" />
         </div>
-        <div className="ParentBtn">
-          <Button variant="contained" className="btn-red Loginbtn">
-            Войти
-          </Button>
+        <div className="d-flex justify-content-center align-items-center">
+          <Link to={SITE_URL.DASHBOARD}>
+            <Button variant="contained" className="btn-red LoginBtn">
+              Войти
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
