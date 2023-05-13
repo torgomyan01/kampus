35f4be402c365c71057a317fcb8ea3ae0a2d8f4e
armanpinachyan <pinachyan99@mail.ru>
2023-05-13 15:50:04 +0400

    ubdate

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import "./login.css";
+import "./Login.css";
 import { Button } from "@mui/material";
+import kompuslogo from "../../assets/images/kompuslogo.png";
 
 function Login() {
   return (
-    <div className="d-flex justify-content-center align-items-center w-100">
-      <div className="def-content">
-        <h3 className="content-title">fgdfgfg</h3>
-        <Button variant="contained" className="btn-red">
-          Подробнее
-        </Button>
+    <div className="d-flex justify-content-center align-items-center Parentlogin">
+      <div className="image">
+        <img src={kompuslogo} alt="" />
+      </div>
+      <div className="def-content Login">
+        <h3 className="content-title text-center">Вход</h3>
+        <p className="LoginTitle text-center">
+          Введите кодовое слово, отправленное вам на <br />
+          <a href="#" className="email text-center">
+            email@example.com
+          </a>
+        </p>
+        <div className="Logininput">
+          <input type="text" placeholder="Кодовое слово" />
+        </div>
+        <div className="ParentBtn">
+          <Button variant="contained" className="btn-red Loginbtn">
+            Войти
+          </Button>
+        </div>
       </div>
     </div>
   );
